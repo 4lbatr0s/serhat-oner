a1f25547e4a71366411a01cf88d9f7a60e61a16e
4lbatr0s <serhatoner@protonmail.com>
2021-10-30 15:02:08 +0300

    ejs implentation, partials, nodemon

--- NodeJS/Homeworks/Week2/CleanBlog/app.js
+++ NodeJS/Homeworks/Week2/CleanBlog/app.js
@@ -1,29 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express') //import express
+const ejs = require('ejs') //import ejs
+
+const app = express() //express türden bir server instance
+
 
 app.use(express.static('public'))//public dosyalarini disari ac.
-app.use(function(req,res,next){
-    console.log('Request:', req.path)
+app.use(function(req,res,next){ //middlewareini olustur.
+    console.log('Request:', req.path) //bir sonraki requestin ne oldugunu anlamamız icin bize yazdır.
     next(); 
 })
 
+app.set("view engine", "ejs"); //hangi template engini kullanacagını bilgisayara bildir.
+
+
 
 app.get('/', (req,res)=> {
-    res.sendFile('index.html', {root:__dirname})
+    res.render('index', {root:__dirname})
 })
 
 
-app.get('/index.html', (req,res)=> {
-    res.sendFile('index.html', {root:__dirname})
-})
-
-app.get('/about.html', (req,res)=> {
-    res.sendFile('about.html', {root:__dirname})
+app.get('/about', (req,res)=> {
+    res.render('about')
 })
 
 
-app.get('/add_post.html', (req,res)=> {
-    res.sendFile('add_post.html', {root:__dirname})
+app.get('/add_post', (req,res)=> {
+    res.render('add_post')
 })
 
 
