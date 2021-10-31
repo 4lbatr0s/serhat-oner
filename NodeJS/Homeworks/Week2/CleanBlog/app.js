37e2e9e7a3266184017f305753e9f9e37457eb92
4lbatr0s <serhatoner@protonmail.com>
2021-10-31 19:54:03 +0300

    I've created aa Post model, inclcluded mongoose 3rd party into my project, changed background images of the blog pages, removed the facebook sign and replaced the fontawesome twitter icon with linkedin icon, embedded my github and linkedin links in these two icons, created 3 blog posts with postman

--- NodeJS/Homeworks/Week2/CleanBlog/app.js
+++ NodeJS/Homeworks/Week2/CleanBlog/app.js
@@ -1,23 +1,39 @@
 const express = require('express') //import express
+const mongoose = require('mongoose')//mongoose import.
 const ejs = require('ejs') //import ejs
+
+const path = require('path')
+const Post = require('./models/Post')
+
+
 
 const app = express() //express türden bir server instance
 
+// connect db
+mongoose.connect('mongodb://localhost/clean-blog-test-db', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
 
-app.use(express.static('public'))//public dosyalarini disari ac.
-app.use(function(req,res,next){ //middlewareini olustur.
-    console.log('Request:', req.path) //bir sonraki requestin ne oldugunu anlamamız icin bize yazdır.
+
+//MIDDLEWARE
+app.use(express.json()) //to send json files.
+app.use(express.static('public'))//in order display static files in the web browser.
+app.use(function(req,res,next){ //
+    console.log('Request:', req.path) //console.log the next request.
     next(); 
 })
 
-app.set("view engine", "ejs"); //hangi template engini kullanacagını bilgisayara bildir.
+app.set("view engine", "ejs"); //tells the browser what view engine we are going to use.
 
 
-
-app.get('/', (req,res)=> {
-    res.render('index', {root:__dirname})
+//routes
+app.get('/', async (req,res)=> {
+    const posts = await Post.find({}) //wait until index page renders the posts.
+    res.render('index', {
+        posts
+    })
 })
-
 
 app.get('/about', (req,res)=> {
     res.render('about')
@@ -28,8 +44,14 @@ app.get('/add_post', (req,res)=> {
     res.render('add_post')
 })
 
+app.post('/posts', async (req, res) => { // async - await yapısı kullanacğız.
+    await Post.create(req.body) // thanks to the Post model, we are turning our request body into a database document.
+    console.log(req.body) 
+    res.redirect('/')
+  });
 
-const port = 3000
+
+const port = 5000
 
 app.listen(port, ()=> {
     console.log('App works on port:', port)
